0462aa395467090a613426f15357c414ef210721
Vince Richese <vrichese@mi-u5.21-school.ru>
2019-06-29 19:38:19 +0300

    buf changed from 10 to 30

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: vrichese <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/18 14:46:30 by vrichese          #+#    #+#             */
-/*   Updated: 2019/06/27 16:20:09 by vrichese         ###   ########.fr       */
+/*   Updated: 2019/06/29 19:35:21 by vrichese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 10
+# define BUFF_SIZE 30
 
 # include <fcntl.h>
 # include "libft.h"
